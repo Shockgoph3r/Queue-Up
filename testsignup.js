5e817581eed4bd77c0fdcf5327c7f82592546d25
kenniky <kennikyofficial@gmail.com>
2016-02-28 13:41:33 -0500

    sign ups connect to server

--- testsignup.js
+++ testsignup.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
 	var firebase = new Firebase('https://queueue.firebaseio.com/');
+
+	$("#signup").click(function(e){
+		e.preventDefault();
+		firebase.once("value",function(snapshot){
+			// console.log("ihf");
+			var username = $("#un").val();
+			var password = $("#pw").val();
+			console.log(username+"___"+password);
+			var ref = firebase.child(username);
+			// console.log("sdfih");
+			ref.set(password);
+			// console.log("hihi");
+		});
+	})
 })
