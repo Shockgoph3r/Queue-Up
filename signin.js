22c439ac2c4c255a9b25a1aec561ee9e81770003
kenniky <kennikyofficial@gmail.com>
2016-02-28 14:00:37 -0500

    signin
    implement it and somehow call function checkLogin(username, password)

--- signin.js
+++ signin.js
@@ -2,8 +2,15 @@ $(document).ready(function(){
 	var firebase = new Firebase('https://queueue.firebaseio.com/');
 
 	function checkLogin(username, password){
-		firebase.once("value",function(snapshot){
-			
+		var ref=firebase.child(username);
+		ref.once("value",function(snapshot){
+			return snapshot.val() == password;
 		});
+		return false;
 	}
+
+	$("#signup").click(function(e){
+		e.preventDefault();
+		checkLogin("sfsf","sfsf");
+	})
 })
