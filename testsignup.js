a38bea3a93b3f1acecd19dd3da155d56fe06d2c7
kenniky <kennikyofficial@gmail.com>
2016-02-28 16:59:47 -0500

    SIGNING IN WORKS BOIIISSS

--- testsignup.js
+++ testsignup.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 	var firebase = new Firebase('https://queueue.firebaseio.com/');
 
+	function checkLogin(username, password, textBack){
+		var ref=firebase.child(username);
+		ref.once("value",function(snapshot){
+			var ret = snapshot.val() == password;
+			// console.log(ret);
+			textBack(ret);
+		});
+	}
+
 	$("#signup").click(function(e){
 		e.preventDefault();
 		// firebase.once("value",function(snapshot){
@@ -12,32 +21,28 @@ $(document).ready(function(){
 			// console.log("sdfih");
 			ref.set(password);
 			// console.log("hihi");
+			alert("Your account was successfully created!");
+			window.location.href = "buttons.html";
 		// });
 	})
 
 	$("#signin").click(function(e){
 		e.preventDefault();
-		// firebase.once("value",function(snapshot){
-			// console.log("ihf");
 			var username = $("#un").val();
 			var password = $("#pw").val();
-			// console.log(username+"___"+password);
-			// var ref = firebase.child(username);
-			// console.log("sdfih");
-			// ref.set(password);
-			// console.log("hihi");
-		// });
-		checkLogin(username,password); //USE THIS TO ACTUALLY SIGN IN!!!!! :OOOOOOO
-	})
+		checkLogin(username,password, function(answer) {
 
-	function checkLogin(username, password){
-		var ref=firebase.child(username);
-		ref.once("value",function(snapshot){
-			var ret = snapshot.val() == password;
-			// console.log(ret);
-			return ret;
+			if(answer){
+				window.location.href = "buttons.html";
+			}else{
+				alert("The username/password combination is invalid :(");
+			} 
 		});
-
-		return false;
-	}
+		//console.log("5"+check);
+		//if(check){
+		//	window.location.href = "buttons.html";
+		//}else{
+		//	alert("The username/password combination is invalid :(");
+		//} //USE THIS TO ACTUALLY SIGN IN!!!!! :OOOOOOO
+	})
 })
