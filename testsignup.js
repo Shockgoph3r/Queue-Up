bc99d245c560da3b0774a7211004fba07f20f492
kenniky <kennikyofficial@gmail.com>
2016-02-28 14:20:47 -0500

    updated with signing in
    NOTE: signin.js is basically useless now loooool

--- testsignup.js
+++ testsignup.js
@@ -7,11 +7,37 @@ $(document).ready(function(){
 			// console.log("ihf");
 			var username = $("#un").val();
 			var password = $("#pw").val();
-			console.log(username+"___"+password);
+			// console.log(username+"___"+password);
 			var ref = firebase.child(username);
 			// console.log("sdfih");
 			ref.set(password);
 			// console.log("hihi");
 		// });
 	})
+
+	$("#signin").click(function(e){
+		e.preventDefault();
+		// firebase.once("value",function(snapshot){
+			// console.log("ihf");
+			var username = $("#un").val();
+			var password = $("#pw").val();
+			// console.log(username+"___"+password);
+			// var ref = firebase.child(username);
+			// console.log("sdfih");
+			// ref.set(password);
+			// console.log("hihi");
+		// });
+		checkLogin(username,password); //USE THIS TO ACTUALLY SIGN IN!!!!! :OOOOOOO
+	})
+
+	function checkLogin(username, password){
+		var ref=firebase.child(username);
+		ref.once("value",function(snapshot){
+			var ret = snapshot.val() == password;
+			// console.log(ret);
+			return ret;
+		});
+
+		return false;
+	}
 })
